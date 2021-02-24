86d3c20f4878e6001e1d0a90d28672bb47a4662a
Emanuel Lucban <emanuel.lucban@berkeley.edu>
2021-02-24 13:32:09 -0800

    working on auth methods

--- pybit/auth.py
+++ pybit/auth.py
@@ -27,7 +27,7 @@ class FitbitAuth(object):
     auth_uri = 'https://www.fitbit.com/oauth2/authorize'
     full_scope = ['activity', 'heartrate', 'location', 'nutrition', 'profile', 'settings', 'sleep', 'social', 'weight']
 
-    def __init__(self, client_id, client_secret, scope=None, access_token=None, expires_in=None, refresh_token=None,
+    def __init__(self, client_id, client_secret, scope=None, access_token=None, expires_dt=None, refresh_token=None,
                  redirect_uri=None):
         self.client_id = client_id
         self.client_secret = client_secret
@@ -40,15 +40,44 @@ class FitbitAuth(object):
             else:
                 raise ValueError('Scope: %s is invalid' % scope)
         self.access_token = access_token
-        self.expires_in = expires_in
+        self.expires_dt = expires_dt
         self.refresh_token = refresh_token
         self.redirect_uri = redirect_uri
 
         # if any of access_token, expires_in, and refresh_token is None, then this is not a pre-authorized user.
         # will have to return the authorization uri, with the requested scope
-        if not all([self.access_token, self.refresh_token, self.expires_in]):
+        if not all([self.access_token, self.refresh_token, self.expires_dt]):
             # Generate authorization URI
             pass
+
+    def generate_auth_url(self):
+        # Authorization code is returned on callback per OAUTH2 spec. auth code must be exchanged within 1 hour
+        uri = self.auth_uri
+        uri = uri + '?response_type=code&client_id=%s' % self.client_id
+        uri = uri + '&redirect_uri=%s' % self.redirect_uri.replace(':', '%3A').replace('/', '%2F')
+        uri = uri + '&scope=%s' % '%20'.join(self.scope)
+        return uri
+
+    def get_access_token(self, auth_code=None):
+        # If no auth_code and have refresh_token, then refresh
+        concat = '%s:%s' % (self.client_id, self.client_secret)
+        header = {'Authorization': 'Basic ' + str(base64.b64encode(concat.encode('ascii')), 'utf-8'),
+                  'Content-Type': 'application/x-www-form-urlencoded'}
+
+        if not auth_code:
+            body = {'client_id': self.client_id,
+                    'grant_type': 'authorization_code',
+                    'redirect_uri': self.redirect_uri,
+                    'code': auth_code}
+        # else refresh
+        else:
+            body = {'grant_type': 'refresh_token',
+                    'refresh_token': self.refresh_token}
+
+        pass
+
+    def revoke_access(self):
+        pass
 
     def _call(self, **kwargs):
         pass
