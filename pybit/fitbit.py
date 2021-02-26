224bf47d511bbfcf833515f247ac29d67bbaaca5
Emanuel Lucban <emanuel.lucban@berkeley.edu>
2021-02-26 00:07:33 -0800

    working on Fitbit class

---
+++ pybit/fitbit.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import datetime
+import pandas as pd
+
+
+class Fitbit(object):
+    api_uri = 'https://api.fitbit.com'
+
+    def __init__(self, fitbit_auth):
+        pass
+
+    def activity(self):
+        pass
+
+    def heartrate(self):
+        pass
+
+    def location(self):
+        pass
+
+    def nutrition(self):
+        pass
+
+    def profile(self):
+        pass
+
+    def settings(self):
+        pass
+
+    def sleep(self):
+        pass
+
+    def social(self):
+        pass
+
+    def weight(self):
+        pass
+
+    def _call(self, path, **kwargs):
+        pass
+
+
+
+
