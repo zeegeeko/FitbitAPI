853378405c0cc9f535c7439e8df76710b9c7fbc8
Emanuel Lucban <emanuel.lucban@berkeley.edu>
2021-03-18 22:28:28 -0700

    split heartrate method to intraday and summary.

--- pybit/fitbit.py
+++ pybit/fitbit.py
@@ -2,7 +2,7 @@ from auth import FitbitAuth
 
 import requests
 import json
-import datetime
+from datetime import datetime
 import pandas as pd
 
 
@@ -18,13 +18,51 @@ class Fitbit(object):
     def activity(self):
         pass
 
-    def heartrate(self, start_date, end_date, data_format='df', *df_kwargs):
+    def heartrate_intraday(self, start_date, end_date, detail='1min', data_format='df', **df_kwargs):
         """
         Fetches intraday time-series heartrate data from Fitbit API.
 
         :param start_date: Start date yyyy-MM-dd
         :param end_date: End date yyyy-MM-dd
+        :param detail: string 1min or 1sec intervals (1min default)
         :param data_format: 'df' for Pandas DataFrame or 'json'
+        :param **df_kwargs: keyworded arguments to pass to Pandas DataFrame
+        :return: json or dataframe of heartrate data
+        """
+        # check detail
+        if detail != '1min' or detail != '1sec':
+            raise ValueError('detail must be either 1min or 1sec')
+
+        # check data_format
+        if data_format != 'df' or data_format != 'json':
+            raise ValueError('data_format must be df or json')
+
+        s_date = datetime.strptime(start_date, '%Y/%m/%d')
+        e_date = datetime.strptime(end_date, '%Y/%m/%d')
+
+        # Note example of REST API call for heartrate data (intraday)
+        # GET https://api.fitbit.com/1/user/[user-id]/activities/heart/date/[date]/[period].json
+        # GET https://api.fitbit.com/1/user/[user-id]/activities/heart/date/[base-date]/[end-date].json
+        # GET https://api.fitbit.com/1/user/-/activities/heart/date/[date]/[end-date]/[detail-level].json
+        # GET https://api.fitbit.com/1/user/-/activities/heart/date/[date]/[end-date]/[detail-level]/time/[start-time]/[end-time].json
+        # GET https://api.fitbit.com/1/user/-/activities/heart/date/[date]/1d/[detail-level].json`
+        # GET https://api.fitbit.com/1/user/-/activities/heart/date/[date]/1d/[detail-level]/time/[start-time]/[end-time].json
+
+        # Since the API only allows intraday data calls for 1 day per call we have to iterate and join multiple calls
+        date_list = []
+        user_uri = '/1/user/%s'
+        activity_uri = '/activities/heart/date/%s/1d/%s.json'
+
+        resp = self._call()
+
+    def heartrate_summary(self, start_date, end_date, data_format='df', **df_kwargs):
+        """
+        Fetches heartrate daily summaries from Fitbit API.
+
+        :param start_date: Start date yyyy-MM-dd
+        :param end_date: End date yyyy-MM-dd
+        :param data_format: 'df' for Pandas DataFrame or 'json'
+        :param **df_kwargs: keyworded arguments to pass to Pandas DataFrame
         :return: json or dataframe of heartrate data
         """
         # Note example of REST API call for heartrate data (intraday)
